08d7b1cbc195ddf363b812eb03f896bcf5f54474
Sophia Guo <sophiag@ca.ibm.com>
2020-04-27 12:51:34 -0400

    initial commit

--- src/build-jdk-hotspot.ts
+++ src/build-jdk-hotspot.ts
@@ -3,8 +3,13 @@ import * as builder from './builder'
 
 async function run(): Promise<void> {
   try {
-    let version = core.getInput('version', {required: false})
-    await builder.buildJDK(version)
+    let javaToBuild = core.getInput('javaToBuild', {required: false})
+    let targetOs = core.getInput('targetOs', {required: false})
+    let architecture = core.getInput('architecture', {required: false})
+    if (!javaToBuild) javaToBuild = 'jdk11u'
+    if (!targetOs) targetOs = 'mac'
+    if (!architecture) architecture = 'x64'
+    await builder.buildJDK(javaToBuild, targetOs, architecture)
   } catch (error) {
     core.setFailed(error.message)
   }
--- src/builder.ts
+++ src/builder.ts
@@ -4,139 +4,66 @@ import * as tc from '@actions/tool-cache'
 import * as io from '@actions/io'
 import { EXDEV } from 'constants'
 
-let linuxContainer = ''
-let linuxBootJDKUri = ''
-let macOSBootJDKUri = ''
-let windowsBootJDKUri = ''
-let buildJavaVersion = ''
-let buildJavaVariant = 'hotspot'
-let buildJavaConfigure = ''
-let freetypeUri = 'https://buildreqs.blob.core.windows.net/freetype/freetype-2.9.1.tar.gz'
-let alsaUri = 'https://buildreqs.blob.core.windows.net/alsa/alsa-lib-1.1.6.tar.bz2'
 const workDir = process.env['GITHUB_WORKSPACE']
-const ADOPT_OPENJDK_BUILD_REPO_URI = `${workDir}/openjdk-build`
-core.info(`workdir is ${workDir}`)
+const dependenciesDir =  `${workDir}/tmp`
+const jdkBootDir = `${workDir}/jdk/boot`
+const javaHomeDir = `${workDir}/jdk/home`
+
 export async function buildJDK(
-  version: string
+  javaToBuild: string,
+  targetOs: string,
+  architecture: string
 ): Promise<void> {
+
+  //set parameters and environment
   await exec.exec(`git clone https://github.com/AdoptOpenJDK/openjdk-build.git`)
-  process.env['JAVA_VESION'] = version
-  process.env['DEBUG_LEVEL'] ='slowdebug'
-  const bootjdkVersion = (parseInt(version) - 1).toString()
-  const bootjdkJar = await tc.downloadTool(`https://api.adoptopenjdk.net/v2/binary/releases/openjdk${bootjdkVersion}?openjdk_impl=openj9&os=linux&arch=x64&release=latest&heap_size=normal&type=jdk`)
-  await io.mkdirP('JDK_BOOT_DIR')
-  await exec.exec('ls')
-  await exec.exec(`sudo tar -xzf ${bootjdkJar} -C ./JDK_BOOT_DIR --strip=1`)
-  await io.rmRF(`${bootjdkJar}`)
-  core.exportVariable('JAVA_HOME', `${workDir}/JDK_BOOT_DIR`)//# Set environment variable JAVA_HOME, and prepend ${JAVA_HOME}/bin to PATH
-  core.addPath(`${workDir}/JDK_BOOT_DIR/bin`)
+  const time = new Date().toISOString().split('T')[0]
+  const fileName = `Open${javaToBuild.toUpperCase()}-jdk_${architecture}_${targetOs}_hotspot_${time}`
   
-  await exec.exec('printenv')
-  process.chdir('openjdk-build')
-  const CONFIG_ARGS = '--with-debug-level=slowdebug --disable-ccache --disable-warnings-as-errors'
-  await exec.exec(`makejdk-any-platform.sh \
-    --adoptopenjdk-build-repo ${ADOPT_OPENJDK_BUILD_REPO_URI} \
-    -J ${workDir}/JDK_BOOT_DIR \
-  --disable-shallow-git-clone \
-  --configure-args "${CONFIG_ARGS}"  \
-  -d artifacts \
-  --target-file-name openjdkbuildtest-linux-x64.tar.gz  \
-  --alsa-tarball-uri ${alsaUri} \
-  --freetype-tarball-uri ${freetypeUri} \
-  ${buildJavaConfigure} \
-  --build-variant ${buildJavaVariant} \
-  --disable-adopt-branch-safety \
-  ${version}`)
-  await exec.exec('find ./ -name openjdkbuildtest-linux-x64.tar.gz')
-}
+  await io.mkdirP('jdk')
+  process.chdir('jdk')
+  await io.mkdirP('boot')
+  await io.mkdirP('home')
 
-/* 
-async function installDependencies(version: string): Promise<void> {
-  await exec.exec('sudo apt-get update')
-  await exec.exec(
-    'sudo apt-get install -qq -y --no-install-recommends \
-    software-properties-common \
-    python-software-properties'
-  )
-//Note gcc-multilib is needed on github environment
-  await exec.exec(`sudo apt-get update`)
-  await exec.exec(
-    'sudo apt-get install -qq -y --no-install-recommends \
-    autoconf \
-    ca-certificates \
-    ccache \
-    cmake \
-    cpio \
-    file \
-    git \
-    git-core \
-    libasound2-dev \
-    libcups2-dev \
-    libdwarf-dev \
-    libelf-dev \
-    libfontconfig1-dev \
-    libfreetype6-dev \
-    libnuma-dev \
-    libx11-dev \
-    libxext-dev \
-    libxrender-dev \
-    libxt-dev \
-    libxtst-dev \
-    make \
-    nasm \
-    pkg-config \
-    realpath \
-    ssh \
-    unzip \
-    wget \
-    gcc-multilib \
-    zip'
-  )
-  await io.rmRF(`/var/lib/apt/lists/*`)
-
-  if (version === '8') {
-    core.info(` are we in correct location? which should be for ${version}`)
-    await exec.exec('sudo add-apt-repository ppa:openjdk-r/ppa')
-    await exec.exec(`sudo apt-get update`)
-    await exec.exec(
-      'sudo apt-get install -qq -y --no-install-recommends openjdk-7-jdk'
-    )
-    await io.rmRF(`/var/lib/apt/lists/*`)
-  } else {
-    await exec.exec(`sudo apt-get update`)
-    await exec.exec(
-      'sudo apt-get install -qq -y --no-install-recommends libxrandr-dev'
-    )
-    await io.rmRF(`/var/lib/apt/lists/*`)
-
-  }
-
-  process.chdir('/usr/local')
-  core.info(`current path is ${process.cwd()}`)
-  const gccBinary = await tc.downloadTool(`https://ci.adoptopenjdk.net/userContent/gcc/gcc730+ccache.x86_64.tar.xz`)
-  core.info(`current path is ${process.cwd()}`)
-  await exec.exec(`ls -l ${gccBinary}`)
-  await exec.exec(`sudo tar -xJ --strip-components=1 -C /usr/local -f ${gccBinary}`)
-  await io.rmRF(`${gccBinary}`)
-
-  await exec.exec(`sudo ln -s /usr/lib/x86_64-linux-gnu /usr/lib64`)
-  await exec.exec(`sudo ln -s /usr/include/x86_64-linux-gnu/* /usr/local/include`)
-  await exec.exec(`sudo ln -sf /usr/local/bin/g++-7.3 /usr/bin/g++`)
-  await exec.exec(`sudo ln -sf /usr/local/bin/gcc-7.3 /usr/bin/gcc`)
   process.chdir(`${workDir}`)
-  const freeMarker = await tc.downloadTool(`https://sourceforge.net/projects/freemarker/files/freemarker/2.3.8/freemarker-2.3.8.tar.gz/download`)
-  await exec.exec(`sudo tar -xzf ${freeMarker} freemarker-2.3.8/lib/freemarker.jar --strip=2`)
-  await io.rmRF(`${freeMarker}`)
+
+  if (process.platform === 'darwin') {
+    await exec.exec('brew install autoconf ccache coreutils')
+  }
+  
+  const bootJDKVersion = getBootJdkVersion(javaToBuild)
+  const bootjdkJar = await tc.downloadTool(`https://api.adoptopenjdk.net/v2/binary/releases/openjdk${bootJDKVersion}?openjdk_impl=hotspot&os=${targetOs}&arch=${architecture}&release=latest&heap_size=normal&type=jdk`)
+  await exec.exec(`sudo tar -xzf ${bootjdkJar} -C ./jdk/boot --strip=3`)
+  await io.rmRF(`${bootjdkJar}`)
+  const jdk8Jar = await tc.downloadTool('https://api.adoptopenjdk.net/v2/binary/releases/openjdk8?os=mac&release=latest&arch=x64&heap_size=normal&type=jdk&openjdk_impl=hotspot')
+  await exec.exec(`sudo tar -xzf ${jdk8Jar} -C ./jdk/home --strip=3`)
+  await io.rmRF(`${jdk8Jar}`)
+  core.exportVariable('JAVA_HOME', `${workDir}/jdk/boot`)//# Set environment variable JAVA_HOME, and prepend ${JAVA_HOME}/bin to PATH
+  core.addPath(`${workDir}/jdk/boot/bin`)
+  process.chdir('openjdk-build')
+  //const CONFIG_ARGS = "--disable-ccache --disable-warnings-as-errors --with-extra-cxxflags='-stdlib=libc++ -mmacosx-version-min=10.8'"
+  await exec.exec(`./makejdk-any-platform.sh \
+  -J ${jdkBootDir} \
+  --disable-shallow-git-clone \
+  --configure-args "--disable-warnings-as-errors --with-extra-cxxflags='-stdlib=libc++ -mmacosx-version-min=10.8'" \
+  -d artifacts \
+  --target-file-name ${fileName}.tar.gz  \
+  --use-jep319-certs \
+  --build-variant hotspot \
+  --disable-adopt-branch-safety \
+  ${javaToBuild}`)
+ 
+  await exec.exec(`find ./ -name ${fileName}.tar.gz`)
 }
 
-async function getSource(
-  openj9Version: string,
-  user: string,
-  branch: string
-): Promise<void> {
-  await exec.exec(`git clone -b ${branch} https://github.com/${user}/${openj9Version}`)
-  process.chdir(`${openj9Version}`)
-  core.info(`current path is ${process.cwd()}`)
-  await exec.exec(`bash ./get_source.sh`)
-  await exec.exec(`bash configure --with-freemarker-jar=${workDir}/freemarker.jar`)
-} */
+function getBootJdkVersion(javaToBuild: string): string {
+  let bootJDKVersion = ''
+  if (`${javaToBuild}` === 'jdk11u') {
+    bootJDKVersion = '10'
+  } else if (`${javaToBuild}` === 'jdk14u') {
+    bootJDKVersion = '13'
+  } else {
+    core.error('not supported jdk version')
+  }
+  return bootJDKVersion
+}
